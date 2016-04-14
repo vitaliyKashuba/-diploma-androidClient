f288f89571f6a15aafb09640c1e8719d84ebc704
Vitaliy <k.vitaliy1994@gmail.com>
2016-04-14 19:40:19 +0300

    simple test for interlayer server

--- app/src/main/java/com/example/vitaliy/diplomaandroidclient/MainActivity.java
+++ app/src/main/java/com/example/vitaliy/diplomaandroidclient/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.vitaliy.diplomaandroidclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    int port = 1235;
+    String ip = "192.168.10.101"; //change it to ip of interlayer server
+
+    String sendMessage;
+    EditText input = (EditText)findViewById(R.id.input);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSendClick(View view)
+    {
+        Thread sendThread = new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Socket socket = new Socket(ip, port);
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                    sendMessage = input.getText().toString();
+                    writer.write(sendMessage);
+                    writer.close();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+        sendThread.run();
     }
 }

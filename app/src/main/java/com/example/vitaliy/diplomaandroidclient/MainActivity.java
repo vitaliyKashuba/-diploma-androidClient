f8c0b815c9e26bebdf8a128370d8bcce65e0f72c
Vitaliy <k.vitaliy1994@gmail.com>
2016-05-24 20:49:32 +0300

    added Message recieving. testing

--- app/src/main/java/com/example/vitaliy/diplomaandroidclient/MainActivity.java
+++ app/src/main/java/com/example/vitaliy/diplomaandroidclient/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OptionalDataException;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import message.Message;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -17,9 +22,13 @@ public class MainActivity extends AppCompatActivity
 
     String sendMessage;
     EditText input;
+    TextView temperatureTextView;
 
     Socket socket;
     PrintWriter writer;
+    ObjectInputStream reader;
+
+    Message message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,6 +36,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         input = (EditText)findViewById(R.id.input);
+        temperatureTextView = (TextView)findViewById(R.id.temperature);
 
         initSocket();
     }
@@ -42,6 +52,8 @@ public class MainActivity extends AppCompatActivity
                 {
                     socket = new Socket(ip, port);
                     writer = new PrintWriter(socket.getOutputStream());
+                    reader = new ObjectInputStream(socket.getInputStream());
+
                 } catch (IOException e)
                 {
                     e.printStackTrace();
@@ -63,5 +75,31 @@ public class MainActivity extends AppCompatActivity
             }
         }).start();
         initSocket();
+    }
+
+    public void messageListener()
+    {
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while(true)
+                {
+                    try
+                    {
+                        message = (Message)reader.readObject();
+                        temperatureTextView.setText((int) message.getTemperature());
+                    }
+                    catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (OptionalDataException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/message/Message.java
@@ -0,0 +1,81 @@
+package message;
+
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ * used to send serialized data from home to interlayerServer
+ */
+public class Message implements Serializable
+{
+    //some data and get-methods
+    private int lightLevel;
+    private float temperature;
+    private float humidity;
+    private byte armStatus; // change to enum?
+    private boolean motionDetected;
+    private boolean relayStatus;
+    private File image;
+    //private File photo; really needs?
+
+    public int getLightLevel()
+    {
+        return lightLevel;
+    }
+
+    public float getTemperature()
+    {
+        return temperature;
+    }
+
+    public byte getArmStatus()
+    {
+        return armStatus;
+    }
+
+    public float getHumidity()
+    {
+        return humidity;
+    }
+
+    public boolean getMotionDetected()
+    {
+        return motionDetected;
+    }
+
+    public boolean getRelayStatus()
+    {
+        return relayStatus;
+    }
+
+    public File getImage()
+    {
+        return image;
+    }
+
+    public Message() //just to test connection, needs to be rewrited
+    {
+        temperature = 20;
+        armStatus = 1;
+    }
+
+    public Message(int lightLevel, float tempetarure, float humidity, boolean motion, boolean relayStatus)
+    {
+        this.lightLevel = lightLevel;
+        this.temperature=tempetarure;
+        this.humidity = humidity;
+        this.motionDetected = motion;
+        this.relayStatus = relayStatus;
+    }
+
+    public Message(int lightLevel, float tempetarure, float humidity, boolean motion, boolean relayStatus, File image)
+    {
+        this.lightLevel = lightLevel;
+        this.temperature=tempetarure;
+        this.humidity = humidity;
+        this.motionDetected = motion;
+        this.relayStatus = relayStatus;
+        this.image = image;
+    }
+
+}
